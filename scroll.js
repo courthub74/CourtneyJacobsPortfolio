026c98f86f8cc9638e00bd3701da522edbbdd089
Courdevelops <courdevelops@gmail.com>
2023-02-24 18:12:11 -0500

    scroll js started

--- scroll.js
+++ scroll.js
@@ -1,6 +1,7 @@
 // Query select each section
-
+const sections = document.querySelectorAll('.section');
 // Loop through the sections
+sections.forEach((el) => observer.observe(el)); 
 // for each one create arrow function observer
     // and call each one an observe
 
