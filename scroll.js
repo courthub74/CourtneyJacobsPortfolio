56d1cfdad5b6514fba6a8663c81c6758b79cc3dc
Courdevelops <courdevelops@gmail.com>
2023-02-26 17:04:32 -0500

    top working but scroll is weird

--- scroll.js
+++ scroll.js
@@ -1,9 +1,17 @@
+// create the intersection observer function
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('section');
+        } else {
+            entry.target.classList.remove('section');
+        }
+    });
+});
+
 // Query select each section
 const sections = document.querySelectorAll('.section');
 // Loop through the sections
 sections.forEach((el) => observer.observe(el)); 
-// for each one create arrow function observer
-    // and call each one an observe
-
-// create the intersection observer function
 
